89fc07d2ad13b4c3ae8bb7cba183d8ca9946acd6
kammagik <kammagik@yahoo.com>
2019-04-07 21:11:35 -0400

    implemented register

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
-
+const bcrypt = require('bcryptjs');
 const { authenticate } = require('../auth/authenticate');
+
+const userDB = require('../database/helpers/userDB')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,7 +12,15 @@ module.exports = server => {
 
 function register(req, res) {
   const user = req.body;
-  user.passwoord = bcrypt.hashSync(user.password, 10)
+  user.password = bcrypt.hashSync(user.password, 12)
+
+  userDB.add(user)
+  .then(id => {
+    res.status(201).json(id)
+  })
+  .catch(err => {
+    res.status(500).json({message:'cannot register user'})
+  })
 }
 
 function login(req, res) {
